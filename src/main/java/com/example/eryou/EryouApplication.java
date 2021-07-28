0c9b49a75c8aa168e595c3fefd19715dafd99347
rex <imsunzhiqi@outlook.com>
2021-07-28 08:57:49 +0800

    Initial commit

---
+++ src/main/java/com/example/eryou/EryouApplication.java
@@ -0,0 +1,13 @@
+package com.example.eryou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EryouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EryouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eryou/EryouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eryou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EryouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
